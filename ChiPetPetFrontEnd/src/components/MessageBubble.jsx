bc1910c91330b691a9e75d9da87817b59838ca2d
Jank Old <cenkolcay22@gmail.com>
2023-12-13 18:34:19 +0300

    Frontend messaging implementation

--- ChiPetPetFrontEnd/src/components/MessageBubble.jsx
+++ ChiPetPetFrontEnd/src/components/MessageBubble.jsx
@@ -1,26 +1,27 @@
 import catImg from "../assets/cat1.jpeg";
 
-
-function MessageBubble(props) {
-    return (
-        <div className="d-flex w-100" style={{justifyContent: props.side % 2 == 0 ? "start" : "end"}}>
-            <div className="card border-primary mb-3 w-50">
-                <div className="d-flex card-header justify-content-start">
-                    <img src={catImg} style={{ width: "50px", borderRadius: "50%", flex: "0 0 auto" }} />
-                    <div style={{ flex: "4 4 auto" }}>
-                        Username
-                    </div>
-                    <div style={{ flex: "1 1 auto" }}>
-                        Date
-                    </div>
-                </div>
-                <hr />
-                <div className="card-body text-primary">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
+function MessageBubble({ username, side, date, content }) {
+  return (
+    <div
+      className="d-flex w-100"
+      style={{ justifyContent: side == 0 ? "start" : "end" }}
+    >
+      <div className="card border-primary mb-3 w-50">
+        <div className="d-flex card-header justify-content-start">
+          <img
+            src={catImg}
+            style={{ width: "50px", borderRadius: "50%", flex: "0 0 auto" }}
+          />
+          <div style={{ flex: "4 4 auto" }}>{username}</div>
+          <div style={{ flex: "1 1 auto" }}>{date}</div>
         </div>
-    )
+        <hr />
+        <div className="card-body text-primary">
+          <p class="card-text">{content}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MessageBubble;
